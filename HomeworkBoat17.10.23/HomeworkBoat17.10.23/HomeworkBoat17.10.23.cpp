cd9bd2684e4e042372b826e7b78e9a24e0e05841
Sasha Erkhov <146490133+SashaErkhov@users.noreply.github.com>
2023-10-15 13:51:43 +0300

    Add files via upload
    Hw17.10.23

---
+++ HomeworkBoat17.10.23/HomeworkBoat17.10.23/HomeworkBoat17.10.23.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::cout << "x = ";
+	double x;
+	std::cin >> x;
+	std::cout << std::endl << "y = ";
+	double y;
+	std::cin >> y;
+	std::cout << std::endl;
+	if ( (x * x + y * y) <= 4.0 )
+	{
+		if ( ((-2.0 <= x <= -1.0) and (y <= 0.0)) or ((1.0 <= x <= 2.0) and (y <= 0.0)) or 
+			( (-1.0<x<=0.0)and(y<=(2.0*x+2.0)) ) or ( (0.0<x<1.0)and(y<=(-2.0*x+2.0)) ) )
+		{
+			std::cout << "You're in the boat!!!" << std::endl;
+		}
+		else
+		{
+			std::cout << "You're not in the boat(((" << std::endl;
+		}
+	}
+	else
+	{
+		std::cout << "You're not in the boat(((" << std::endl;
+	}
+}
---
+++ HomeworkElki10.10.2023/HomeworkElki10.10.2023/HomeworkElki10.10.2023.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+void fooSpaceOut(int count)
+{
+	for (int i = 0;i < count;++i)
+	{
+		std::cout << ' ';
+	}
+}
+void fooStarOut(int count)
+{
+	for (int i = 0;i < count;++i)
+	{
+		std::cout << '*';
+	}
+}
+
+int main()
+{
+	std::cout << "Input count elok: ";
+	int countEli;
+	std::cin >> countEli;
+	std::cout << "Input underground: ";
+	int underground;
+	std::cin >> underground;
+	int now = 1;
+	int space = underground / 2;
+	int lavel = 7;
+	int count = 0;
+	while (now <= underground)
+	{
+		for (int i = 0;i < (countEli - 1);++i)
+		{
+			fooSpaceOut(space);
+			fooStarOut(now);
+			fooSpaceOut(underground + 5 - count);
+		}
+		fooSpaceOut(space);
+		fooStarOut(now);
+		++count;
+		++now;
+		++now;
+		--space;
+		std::cout << std::endl;
+		if (lavel == now)
+		{
+			now = 1;
+			lavel += 4;
+			space = underground / 2;
+			count = 0;
+		}
+	}
+	return 0;//I'm win!
+}
