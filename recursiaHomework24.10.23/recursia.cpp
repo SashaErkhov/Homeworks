3bcdad32731b9332b0205f5ab08a4ab05a03fed6
Sasha Erkhov <146490133+SashaErkhov@users.noreply.github.com>
2023-10-24 02:56:13 +0300

    Add files via upload
    Update

---
+++ recursiaHomework24.10.23/recursia.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main()
+{
+	short int input;
+	//std::cout << std::endl<<sizeof(input)<<std::endl;
+	std::cin >> input;
+	int len;
+	std::cin >> len;
+	input = (input >> len)|(input <<(sizeof(input)*8-len));
+	std::cout << input;
+	return 0;
+}
